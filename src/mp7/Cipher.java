c053130f3e7fa4fbfe4b8d83925d8c85c9f534d9
chipshark <csharma4@illinois.edu>
2017-12-04 11:09:03 -0600

    Added splicer

--- src/mp7/Cipher.java
+++ src/mp7/Cipher.java
@@ -61,7 +61,10 @@ public class Cipher {
 		}
 		return output;
 	}
-	
+
+	public String addSplicer(final String message, final String splicer) {
+		return message + splicer;
+	}
 
 	/**
 	 * Encrypt
